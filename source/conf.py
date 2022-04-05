3d0b8d30f4bd0540a5de3bfcd2fa559df1ad55c9
Clarmy Lee <clarmylee92510@gmail.com>
2022-04-05 22:41:16 +0800

    版本更新到1.0.1

--- source/conf.py
+++ source/conf.py
@@ -22,8 +22,8 @@ copyright = '2022, Clarmy'
 author = 'Clarmy'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
-version = '1.0.0'
+release = '1.0.1'
+version = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 
